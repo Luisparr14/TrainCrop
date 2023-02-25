466a22b5f048abf8a8647fdf58f140ea620921a3
Luis Angel <luisparr14@gmail.com>
2023-02-25 12:30:35 -0500

    action: Update DropZone COmponente

--- src/components/DropZone.jsx
+++ src/components/DropZone.jsx
@@ -1,20 +1,12 @@
-import React, { useCallback } from 'react'
+import React from 'react'
 import { useDropzone } from 'react-dropzone'
-import UploadLogo from './UploadLogo'
+import propTypes from 'prop-types'
 
 const ACCEPTED_FILE_TYPES = {
-  'image/*': ['.jpg', '.jpeg', '.png']
+  'image/*': ['.jpg', '.jpeg', '.png', '.webp']
 }
 
-function MyDropzone () {
-  const onDrop = useCallback((acceptedFiles) => {
-    console.log(acceptedFiles)
-  }, [])
-
-  const onDropRejected = useCallback((rejectedFiles) => {
-    console.log(rejectedFiles)
-  }, [])
-
+function MyDropzone ({ onDrop, onDropRejected }) {
   const { getRootProps, getInputProps, isDragActive } = useDropzone({
     onDrop,
     onDropRejected,
@@ -25,13 +17,12 @@ function MyDropzone () {
     <div {...getRootProps()} className="m-3">
       <input {...getInputProps()} />
       <div className="max-w-xl">
-        <label className={`flex justify-center w-96 h-32 px-4 transition bg-primary-300 border-2 border-dashed rounded-md appearance-none cursor-pointer focus:outline-none hover:border-gray-400 ${isDragActive ? 'border-blue-900' : 'border-accent-200'}`}>
+        <label className={`flex justify-center sm:w-96 h-32 px-4 transition bg-primary-300 border-2 border-dashed rounded-md appearance-none cursor-pointer focus:outline-none hover:border-gray-400 ${isDragActive ? 'border-blue-900' : 'border-accent-200'}`}>
           <span className="flex items-center space-x-2">
-            <UploadLogo />
             <span className="font-medium text-gray-900 flex flex-col justify-center items-center">
-            {isDragActive
-              ? <p>Drop the files here ...</p>
-              : <>
+              {isDragActive
+                ? <p>Drop the files here ...</p>
+                : <>
                   <p>Drop files to Attach, or</p>
                   <span className="text-gray-900 mt-4 font-bold bg-purple-600 p-2 rounded-lg">browse files</span>
                 </>
@@ -45,3 +36,8 @@ function MyDropzone () {
 }
 
 export default MyDropzone
+
+MyDropzone.propTypes = {
+  onDrop: propTypes.func.isRequired,
+  onDropRejected: propTypes.func.isRequired
+}
