73132f6ea4b85e770520420b84a4dd56d12dba1b
Luis Angel <luisparr14@gmail.com>
2023-02-25 14:02:20 -0500

    action: Update Dropzone styles

--- src/components/DropZone.jsx
+++ src/components/DropZone.jsx
@@ -14,23 +14,21 @@ function MyDropzone ({ onDrop, onDropRejected }) {
   })
 
   return (
-    <div {...getRootProps()} className="m-3">
+    <div {...getRootProps()} className="my-3 w-full">
       <input {...getInputProps()} />
-      <div className="max-w-xl">
-        <label className={`flex justify-center sm:w-96 h-32 px-4 transition bg-primary-300 border-2 border-dashed rounded-md appearance-none cursor-pointer focus:outline-none hover:border-gray-400 ${isDragActive ? 'border-blue-900' : 'border-accent-200'}`}>
-          <span className="flex items-center space-x-2">
-            <span className="font-medium text-gray-900 flex flex-col justify-center items-center">
-              {isDragActive
-                ? <p>Drop the files here ...</p>
-                : <>
-                  <p>Drop files to Attach, or</p>
-                  <span className="text-gray-900 mt-4 font-bold bg-purple-600 p-2 rounded-lg">browse files</span>
+      <label className={`flex justify-center w-full h-32 px-4 transition bg-primary-300 border-2 border-dashed rounded-md appearance-none cursor-pointer focus:outline-none hover:border-gray-400 ${isDragActive ? 'border-blue-900' : 'border-accent-200'}`}>
+        <span className="flex items-center space-x-2">
+          <span className="font-medium text-gray-900 flex flex-col justify-center items-center">
+            {isDragActive
+              ? <p>Drop all files that you want here</p>
+              : <>
+                  <p>Drop one or more files here, or click to select files</p>
+                  <span className="text-gray-900 mt-4 font-bold bg-primary-200 p-2 rounded-lg">browse files</span>
                 </>
-              }
-            </span>
+            }
           </span>
-        </label>
-      </div>
+        </span>
+      </label>
     </div>
   )
 }
