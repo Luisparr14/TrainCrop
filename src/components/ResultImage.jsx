559716967a137150f29bd5c3483e6c11fd370022
Luis Angel <luisparr14@gmail.com>
2023-02-26 17:01:59 -0500

    add: option to delete Image

--- src/components/ResultImage.jsx
+++ src/components/ResultImage.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import propTypes from 'prop-types'
 
-function ResultImage ({ publicId, width, height }) {
+function ResultImage ({ publicId, width, height, onDeleteImage }) {
   return (
-    <div>
+    <div className='relative'>
+      <span title='Delete Image' onClick={() => onDeleteImage(publicId)} className="bg-red-100 text-red-800 text-xs font-medium px-2.5 py-0.5 rounded dark:bg-red-900 dark:text-red-300 absolute right-0 m-0 cursor-pointer select-none">メ</span>
       <a
         href={`https://res.cloudinary.com/luisparr14/image/upload/w_${width},h_${height},c_thumb,g_faces/${publicId}`}
         download
@@ -14,7 +15,7 @@ function ResultImage ({ publicId, width, height }) {
         key={publicId}
         src={`https://res.cloudinary.com/luisparr14/image/upload/w_${width},h_${height},c_thumb,g_faces/${publicId}`}
         alt='uploaded'
-        className='h-44 object-contain m-2 rounded-lg'/>
+        className='h-44 object-contain rounded-lg'/>
       </a>
     </div>
   )
@@ -25,10 +26,12 @@ export default ResultImage
 ResultImage.propTypes = {
   publicId: propTypes.string.isRequired,
   width: propTypes.number,
-  height: propTypes.number
+  height: propTypes.number,
+  onDeleteImage: propTypes.func
 }
 
 ResultImage.defaultProps = {
   width: 512,
-  height: 512
+  height: 512,
+  onDeleteImage: () => console.log('onDeleteImage')
 }
