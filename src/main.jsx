4427668c3ff1d424a60541c865232ea2dd6f0be5
Luis Angel <luisparr14@gmail.com>
2023-02-28 22:26:57 -0500

    use promises

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-await nets.ssdMobilenetv1.loadFromUri('/models')
+nets.ssdMobilenetv1.loadFromUri('/models').then(() => {
+  console.log('Loaded model')
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
