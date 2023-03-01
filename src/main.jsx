91086e6458abdb9887a87e24cbfa2a365892cfec
Luis Angel <luisparr14@gmail.com>
2023-02-28 22:23:43 -0500

    fix: load model once

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
+import { nets } from 'face-api.js'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+
+await nets.ssdMobilenetv1.loadFromUri('/models')
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- src/utils/detectFaces.js
+++ src/utils/detectFaces.js
@@ -1,9 +1,7 @@
-import { fetchImage, nets, detectAllFaces } from 'face-api.js'
+import { fetchImage, detectAllFaces } from 'face-api.js'
 
 export const detectFaces = async (file) => {
   try {
-    await nets.ssdMobilenetv1.loadFromUri('/models')
-
     const image = await fetchImage(URL.createObjectURL(file))
     const detections = await detectAllFaces(image)
     const numFaces = detections.length
