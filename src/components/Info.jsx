2d6cc5beb333d78b12ec699a6694025e90b0c0bb
Luis Angel <luisparr14@gmail.com>
2023-03-04 16:17:57 -0500

    select none

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 
 function Info ({ onClick }) {
   return (
-    <div className="w-9 h-9 absolute right-2 top-2 cursor-pointer" onClick={onClick}>
+    <div className="w-9 h-9 absolute right-2 top-2 cursor-pointer select-none" onClick={onClick}>
       <img className="w-full h-full relative" src="https://res.cloudinary.com/luisparr14/image/upload/v1677962105/Iconos/g6j8fcgrlw4zmtbk4ix6.png" alt="info" />
     </div>
   )
