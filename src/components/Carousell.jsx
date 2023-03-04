0a3d232cc8ebfa891d02d79dcef0b3100a6f166d
Luis Angel <luisparr14@gmail.com>
2023-03-04 13:02:18 -0500

    separate instructions

--- src/components/Carousell.jsx
+++ src/components/Carousell.jsx
@@ -1,74 +1,12 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-const INSTRUNCTIONS = [
-  {
-    title: 'Upload your images',
-    description: 'Drag and drop your images or click on the button to select them',
-    type: 'video',
-    source: 'https://res.cloudinary.com/luisparr14/video/upload/v1677944606/TRAINCROP%20Instructions/vfdujpqrxazerltgrpod.mp4'
-  },
-  {
-    title: 'Choose your options',
-    description: 'Choose the size of your images',
-    type: 'video',
-    source: 'https://res.cloudinary.com/luisparr14/video/upload/v1677944712/TRAINCROP%20Instructions/vynb7q45qip0lqdwncni.mp4'
-  },
-  {
-    title: 'Download your images',
-    description: 'Click on the download button to download your images or click on the image to see it and if you want to download it',
-    type: 'video',
-    source: 'https://res.cloudinary.com/luisparr14/video/upload/v1677944698/TRAINCROP%20Instructions/aehocu35dgnp6yovxfnj.mp4'
-  },
-  {
-    title: 'Delete your images',
-    description: 'Click on the delete button to delete all your images or in the corner there is a button to delete one image ❌',
-    type: 'video',
-    source: 'https://res.cloudinary.com/luisparr14/video/upload/v1677944581/TRAINCROP%20Instructions/c8gsssmh8ijusz7mzfv2.mp4'
-  },
-  {
-    title: 'Don\'t touch the localStorage',
-    description: 'We use localStorage to save your images (id\'s really) and if you delete it, you will lose all your images',
-    type: 'image',
-    source: 'https://res.cloudinary.com/luisparr14/image/upload/v1677947357/TRAINCROP%20Instructions/i5qcfmhehftuynppcdde.png'
-  },
-  {
-    title: 'We only cut human faces',
-    description: 'If you upload an image with a face that is not human, the image will be rejected',
-    type: 'image',
-    source: 'https://res.cloudinary.com/luisparr14/image/upload/c_scale,w_350/v1677949064/TRAINCROP%20Instructions/osf3qrbr3vhfl17esewx.png'
-  },
-  {
-    title: 'Only one face per image',
-    description: 'If you upload an image with more than one face, the image will be rejected',
-    type: 'image',
-    source: 'https://res.cloudinary.com/luisparr14/image/upload/c_scale,w_350/v1677950007/TRAINCROP%20Instructions/kukfntp8upyzvnqh2ydi.png'
-  },
-  {
-    title: 'When you see the red color',
-    description: 'It means that all your images have been rejected',
-    type: 'image',
-    source: ''
-  },
-  {
-    title: 'When you see the yellow color',
-    description: 'It means that some of your images have been rejected',
-    type: 'image',
-    source: ''
-  },
-  {
-    title: 'Enjoy',
-    description: 'Enjoy the app and if you have any questions, you can contact me',
-    type: 'image',
-    source: null
-  }
-]
-
-function Carousell () {
+function Carousell ({ options }) {
   return (
     <div id="animation-carousel" className="relative sm:text-white" data-carousel="static">
       {/* <!-- Carousel wrapper --> */}
       <div className="relative h-[27rem] overflow-hidden rounded-lg sm:h-[32rem] md:h-[34rem]">
-        {INSTRUNCTIONS.map((instruction, index) => {
+        {options.map((instruction, index) => {
           const { title, description, type, source } = instruction
           return (
             <div key={index} className="hidden duration-200 ease-linear overflow-y-auto" data-carousel-item>
@@ -80,8 +18,8 @@ function Carousell () {
                 {source !== '' && (
                   <section className='w-full flex justify-center'>
                     {type === 'video'
-                      ? <video className="w-full h-full" autoPlay loop>
-                        <source src={source} type="video/mp4" />
+                      ? <video className="w-full h-full" autoPlay={true}>
+                        <source autoPlay src={source} type="video/mp4" />
                       </video>
                       : <img src={source} alt={title} className="" />
                     }
@@ -110,3 +48,12 @@ function Carousell () {
 }
 
 export default Carousell
+
+Carousell.propTypes = {
+  options: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
+    source: PropTypes.string.isRequired
+  })).isRequired
+}
---
+++ src/data/instructions.js
@@ -0,0 +1,62 @@
+export const INSTRUCTIONS = [
+  {
+    title: 'Upload your images',
+    description: 'Drag and drop your images or click on the button to select them',
+    type: 'video',
+    source: 'https://res.cloudinary.com/luisparr14/video/upload/v1677944606/TRAINCROP%20Instructions/vfdujpqrxazerltgrpod.mp4'
+  },
+  {
+    title: 'Choose your options',
+    description: 'Choose the size of your images',
+    type: 'video',
+    source: 'https://res.cloudinary.com/luisparr14/video/upload/v1677944712/TRAINCROP%20Instructions/vynb7q45qip0lqdwncni.mp4'
+  },
+  {
+    title: 'Download your images',
+    description: 'Click on the download button to download your images or click on the image to see it and if you want to download it',
+    type: 'video',
+    source: 'https://res.cloudinary.com/luisparr14/video/upload/v1677944698/TRAINCROP%20Instructions/aehocu35dgnp6yovxfnj.mp4'
+  },
+  {
+    title: 'Delete your images',
+    description: 'Click on the delete button to delete all your images or in the corner there is a button to delete one image ❌',
+    type: 'video',
+    source: 'https://res.cloudinary.com/luisparr14/video/upload/v1677944581/TRAINCROP%20Instructions/c8gsssmh8ijusz7mzfv2.mp4'
+  },
+  {
+    title: 'Don\'t touch the localStorage',
+    description: 'We use localStorage to save your images (id\'s really) and if you delete it, you will lose all your images',
+    type: 'image',
+    source: 'https://res.cloudinary.com/luisparr14/image/upload/v1677947357/TRAINCROP%20Instructions/i5qcfmhehftuynppcdde.png'
+  },
+  {
+    title: 'We only cut human faces',
+    description: 'If you upload an image with a face that is not human, the image will be rejected',
+    type: 'image',
+    source: 'https://res.cloudinary.com/luisparr14/image/upload/c_scale,w_350/v1677949064/TRAINCROP%20Instructions/osf3qrbr3vhfl17esewx.png'
+  },
+  {
+    title: 'Only one face per image',
+    description: 'If you upload an image with more than one face, the image will be rejected',
+    type: 'image',
+    source: 'https://res.cloudinary.com/luisparr14/image/upload/c_scale,w_350/v1677950007/TRAINCROP%20Instructions/kukfntp8upyzvnqh2ydi.png'
+  },
+  {
+    title: 'When you see the red color',
+    description: 'It means that all your images have been rejected',
+    type: 'image',
+    source: ''
+  },
+  {
+    title: 'When you see the yellow color',
+    description: 'It means that some of your images have been rejected',
+    type: 'image',
+    source: ''
+  },
+  {
+    title: 'Enjoy of this app',
+    description: 'Enjoy the app and if you have any questions, you can contact me',
+    type: 'image',
+    source: 'https://res.cloudinary.com/luisparr14/image/upload/c_scale,w_400/v1677952620/TRAINCROP%20Instructions/tnmv5o8xflgzbnub9ziv.gif'
+  }
+]
